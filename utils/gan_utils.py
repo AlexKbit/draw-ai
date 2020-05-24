bb2fade58c5733c332db14c672357584c7e78e77
AlexKbit <sav4enkoaleksandr@gmail.com>
2020-05-24 16:52:58 +0300

    Create README.md

--- utils/gan_utils.py
+++ utils/gan_utils.py
@@ -15,7 +15,7 @@ def make_generator_model():
     model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
+    assert model.output_shape == (None, 7, 7, 256)
 
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     assert model.output_shape == (None, 7, 7, 128)
